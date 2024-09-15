9e5ed506c2f04366b3833e0fdd9ccb2fa0e2afc1
Chelsea W. <80985676+cheollie@users.noreply.github.com>
2024-09-15 11:38:44 -0400

    upload

---
+++ gen.py
@@ -0,0 +1,33 @@
+resting_heart_rate = 65
+age = 25
+max_heart_rate = 220 - age
+
+#heart rate, steps, sleep, vo2 max, calories
+
+#during exercise
+
+#non intensive exercise
+
+#needs exercise
+#if time is 7pm
+time = #get
+if time == 19:
+    if goal_calories_burned < calories_burned:
+        l[4] = goal_calories_burned - calories_burned
+    if goal_steps < steps:
+        l[1] = goal_steps - steps
+    if sleep < 8:
+        l[2] = 8 - sleep
+    
+
+
+    print("You need to exercise")
+
+def post_workout:
+    if heart_rate < resting_heart_rate:
+        l[0] = resting_heart_rate - heart_rate
+    if vo2_max < 30:
+        l[3] = 30 - vo2_max
+
+yourname is tim arm workout
+if left right noticed yapping notified
